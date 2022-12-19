510581cb877cd3ce1d88fb1e2315b885646245f1
William Clarke <wmmclarke@gmail.com>
2022-12-19 22:56:50 +0000

    build tree from input (no interesting sizes though)

--- 7/main.exs
+++ 7/main.exs
@@ -1,11 +1,7 @@
 data = File.read!("input.txt")
-
 lines = String.split(data, "\n")
 
-# a = [1, 2, 3]
-
-IO.puts("heyl")
-IO.puts(List.first(lines))
+# Enum.reduce(lines, %State{}, &State.run/2)
 
 defmodule Tree do
   @behaviour Access
@@ -20,6 +16,10 @@ defmodule Tree do
             # Map of dir_names to Trees
             children: %{}
 
+  def omg do
+    Enum.reduce(lines(), %State{}, &State.run!/2)
+  end
+
   def lines do
     data = File.read!("input.txt")
     lines = String.split(data, "\n")
@@ -28,6 +28,12 @@ defmodule Tree do
 end
 
 defmodule State do
+  @behaviour Access
+  defdelegate get(state, key, default), to: Map
+  defdelegate fetch(state, key), to: Map
+  defdelegate get_and_update(state, key, func), to: Map
+  defdelegate pop(state, key), to: Map
+
   defstruct tree: %Tree{},
             current_working_tree: [],
             is_listing: false
@@ -60,36 +66,15 @@ defmodule State do
     }
   end
 
-  def run(%State{is_listing: true} = state, "dir " <> child_dir_name) do
-    state
+  def run!(line, state) do
+    IO.puts(inspect(line))
+    State.run(line, state)
   end
 
-  def run(%State{is_listing: true} = state, sizeAndChildDirName) do
-    captures = Regex.named_captures(~r/(?<size>\w+) (?<child_dir_name>\w+)/, sizeAndChildDirName)
-    State.add_child(state, captures.size, captures.child_dir_name)
-  end
+  def run("$ cd " <> dir, state) do
+    IO.puts(dir)
+    IO.puts("CDCDCD")
 
-  def add_child(state, size, child_dir_name) do
-    child_tree = %Tree{name: child_dir_name, dir_size: size}
-    tree = state.tree
-    # TODO: more here. Deep insert based on the state current_working_tree
-    # children = tree.children
-    # tree.children = [child_tree | children]
-    %State{state | tree: tree}
-  end
-
-  def run(state, line) do
-    state
-  end
-
-  def run(state, "$ ls") do
-    case state.is_listing do
-      true -> raise "cannot list while listing"
-      false -> %State{state | is_listing: true}
-    end
-  end
-
-  def run(state, "$ cd " <> dir) do
     newState =
       case dir do
         "/" ->
@@ -102,7 +87,50 @@ defmodule State do
           %State{state | current_working_tree: state.current_working_tree ++ [directory]}
       end
 
+    IO.puts("CDCDCD")
+    IO.puts(newState.current_working_tree)
     %State{newState | is_listing: false}
+  end
+
+  def run("$ ls", state) do
+    case state.is_listing do
+      true -> raise "cannot list while listing"
+      false -> %State{state | is_listing: true}
+    end
+  end
+
+  def run("dir " <> child_dir_name, %State{is_listing: true} = state) do
+    add_child(state, 0, child_dir_name)
+  end
+
+  def run(size_and_child_dir_name, %State{is_listing: true} = state) do
+    captures =
+      Regex.named_captures(~r/(?<dir_size>\w+) (?<child_dir_name>\w+)/, size_and_child_dir_name)
+
+    # IO.puts(inspect(state))
+    # IO.puts(inspect(captures))
+
+    add_child(
+      state,
+      Map.fetch!(captures, "dir_size"),
+      Map.fetch!(captures, "child_dir_name")
+    )
+  end
+
+  # def run(state, line) do
+  # state
+  # end
+
+  defp add_child(state, size, child_dir_name) do
+    child_tree = %Tree{name: child_dir_name, dir_size: size}
+
+    path =
+      Enum.intersperse([:tree | state.current_working_tree], :children) ++
+        [:children, child_dir_name]
+
+    IO.puts("path: " <> inspect(path))
+
+    put_in(state, path, child_tree)
   end
 
   defp pop(list) do
